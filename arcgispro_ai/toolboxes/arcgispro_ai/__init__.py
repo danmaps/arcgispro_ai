ae95c1d9ecc2858e22970c3a2bf4134a705ccf15
Danny McVey <dannybmcvey@gmail.com>
2025-05-27 12:15:30 -0700

    fix init

--- arcgispro_ai/toolboxes/arcgispro_ai/__init__.py
+++ arcgispro_ai/toolboxes/arcgispro_ai/__init__.py
@@ -3,13 +3,17 @@ from .core import *
 
 # Import arcpy-dependent components only when explicitly requested
 import arcpy
-# import the pyt from: %localappdata%\ESRI\conda\envs\<your_env>\Lib\site-packages\arcgispro_ai\toolboxes
-# this should resolve to something like "C:\Users\danny\AppData\Local\ESRI\conda\envs\arcgispro-py3-clone\Lib\site-packages\arcgispro_ai\toolboxes\arcgispro_ai_tools.pyt"
+import os
+import sys
 
-env_path = arcpy.env.activeEnvironment.path
+# Get the path to the toolbox using a relative path from this file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)  # Go up one level to the toolboxes directory
+arcgispro_ai_tools_path = os.path.join(parent_dir, "arcgispro_ai_tools.pyt")
 
-# get the path to the arcgispro_ai_tools.pyt file
-arcgispro_ai_tools_path = os.path.join(env_path, "Lib", "site-packages", "arcgispro_ai", "toolboxes", "arcgispro_ai_tools.pyt")
+# Check if the toolbox file exists
+if not os.path.exists(arcgispro_ai_tools_path):
+    raise FileNotFoundError(f"Toolbox file not found at: {arcgispro_ai_tools_path}")
 
-# import the toolbox
-arcpy.importToolbox(arcgispro_ai_tools_path, "arcgispro_ai_tools.pyt")
+# Import the toolbox
+arcpy.ImportToolbox(arcgispro_ai_tools_path, "arcgispro_ai_tools")
