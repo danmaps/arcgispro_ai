58cd88cda857c8aaa12a12b23bb871e684220f19
danmaps <dannybmcvey@gmail.com>
2025-06-13 15:29:39 -0700

    Bump version to 0.0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="arcgispro_ai",
-    version="0.0.9",
+    version="0.0.10",
     author="Danny McVey",
     author_email="dannybmcvey@gmail.com",
     description="AI tools for ArcGIS Pro",
