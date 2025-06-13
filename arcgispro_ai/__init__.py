0116898c786abe2a278cda5141b8ed4bad22c165
danmaps <dannybmcvey@gmail.com>
2025-06-13 15:40:44 -0700

    version bump

--- arcgispro_ai/__init__.py
+++ arcgispro_ai/__init__.py
@@ -2,7 +2,7 @@
 ArcGIS Pro AI Tools
 """
 
-__version__ = "0.0.7"
+__version__ = "0.0.12"
 __author__ = "Danny McVey"
 __email__ = "dannybmcvey@gmail.com"
 __description__ = "AI tools for ArcGIS Pro"
